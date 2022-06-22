b983e6d81b087d6b795344cc17a6ddb22dc95107
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-06-22 08:11:10 +0300

    Update File Structure

--- script.py
+++ script.py
@@ -11,8 +11,6 @@ class Content():
         self.weights = [str(self.indices[i]) + ',' for i in range(1, 45)]
         self.values = [str(random.randint(500, 1500)) + '\n' for _ in range(45)]
     
-
-    
     def generateContent(self, fileName, itemNum):
         """A method for generating and writing content."""
         self.maxWeight = str(random.randint(70, 400)) + '\n'
@@ -32,15 +30,15 @@ class Content():
 
     def main(self):
         """Main method of the class."""
-        f = open("random_items.txt")
+        f = open("Items/random_items.txt")
         lines = f.readlines()
         for line in lines:
             self.items.append(line.rstrip() + ',')
         f.close()
 
-        self.generateContent("items_10.txt", 10)
-        self.generateContent("items_15.txt", 15)
-        self.generateContent("items_20.txt", 20)
+        self.generateContent("Items/items_10.txt", 10)
+        self.generateContent("Items/items_15.txt", 15)
+        self.generateContent("Items/items_20.txt", 20)
 
 if __name__ == "__main__":
     content = Content()
