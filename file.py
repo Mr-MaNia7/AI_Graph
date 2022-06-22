78876f6302f35a3370884ccd9ec1322af511dc1c
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-06-22 11:35:55 +0300

    Improve file handling and displaying

--- file.py
+++ file.py
@@ -1,11 +1,10 @@
 class File():
-    def __init__(self, fname) -> None:
+    def __init__(self) -> None:
         self.maxWeight = 0
         self.items = []
         self.weights = []
         self.values = []
         self.maxChoice = 3
-        self.process(fname)
     
     def process(self, fname):
         f = open(fname)
@@ -15,21 +14,24 @@ class File():
                 continue
             line = line.rstrip()
             if line.find(',') == -1:
-                self.maxWeight = line
+                self.maxWeight = int(line)
                 continue
             words = line.split(',')
             self.items.append(words[0])
-            self.weights.append(words[1])
-            self.values.append(words[2])
+            self.weights.append(int(words[1]))
+            self.values.append(int(words[2]))
+
+        return (self.maxWeight, self.items, self.weights, self.values)
     
     def displayResult(self):
-        print(self.maxWeight, self.items, self.weights, self.values, sep = "\n")
-
-
-class Driver():
-    def __init__(self) -> None:
-        pass
+        print()
+        print("Maximum Weight Limit =>", self.maxWeight) 
+        print("Items List =>", self.items)     
+        print("Weights List =>", self.weights) 
+        print("Values List =>", self.values)
+        print()
 
 if __name__ == "__main__":
-    file = File("items_10.txt")
+    file = File()
+    file.process("Items/items_10.txt")
     file.displayResult()
